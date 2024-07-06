9c5598103280e2e76b5c95c38de7266dc9c1e998
Joshua Janric Antonio <joshuajanric0170@gmail.com>
2024-07-06 15:45:51 +0800

    5th

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'] );
 
-Route::get('/feed', function () {
-    return view('feed');
-});
-
-Route::get('/profile', function () {
-    return view('profile');
-});
+Route::get('/profile', [ProfileController::class, 'index'] );
