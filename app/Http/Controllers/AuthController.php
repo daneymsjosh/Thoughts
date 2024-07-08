da3dde493a2e6e70d740274e9c0202c9c6677113
Joshua Janric Antonio <joshuajanric0170@gmail.com>
2024-07-08 16:52:01 +0800

    21st

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    // Register user
     public function register()
     {
         return view('auth.register');
@@ -30,5 +31,42 @@ class AuthController extends Controller
         ]);
 
         return redirect()->route('dashboard')->with('success', 'Account Created Successfuly!');
+    }
+
+    // Login user
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        // Validate user
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        // Login user
+        if (auth()->attempt($validated)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('dashboard')->with('success', 'Logged In Successfuly!');
+        }
+
+        return redirect()->route('login')->withErrors([
+            'email' => 'Invalid Credentials'
+        ]);
+    }
+
+    // Logout user
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('dashboard')->with('success', 'Logged Out Successfuly!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,11 @@ Route::post('/thoughts/{thought}/comments', [CommentController::class, 'store'])
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/register', [AuthController::class, 'store']);
+
+// Login
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+Route::post('/login', [AuthController::class, 'authenticate']);
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
